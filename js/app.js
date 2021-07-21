89e6971e2072667ecf3fba679c445e7d25cc617c
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-07-21 17:59:54 -0400

    html structure

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const Slider = document.querySelector('.slider');
+const ContainerSlider = document.querySelector('.container-slider');
+
+
+const HeightSlider= '80rem';
+const NumberSliders = 4
+
+Slider.style.width= `${100 * NumberSliders}%`;
+ContainerSlider.style.height = `${HeightSlider}`;
