309802b1a5ffbfdc93eede55bcf8d3cf1fb968d7
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-07-22 02:38:11 -0400

    seguimiento de los dots

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ function iniciarContador() {
 function detenerContador() {
     clearInterval(SliderAuto);
 }
+
 const Slider = document.querySelector('.slider');
 const ContainerSlider = document.querySelector('.container-slider');
 const ItemsSlider = document.querySelectorAll('.item-slider');
@@ -28,21 +29,21 @@ Slider.insertAdjacentElement('afterbegin', LastItemSlider);
 function SliderNext(){
     let FirstItemSlider = document.querySelector('.item-slider');
     Slider.style.marginLeft = '-200%';
-    Slider.style.transition = 'all 2s';
-    
-    
-    CurrentDot+= 1;
-    
-    console.log(CurrentDot);
+    Slider.style.transition = 'all 2s';    
+    CurrentDot+= 1;    
     setTimeout(() => {
         Slider.style.transition = 'none';
         Slider.insertAdjacentElement('beforeend', FirstItemSlider);
         Slider.style.marginLeft = '-100%'
     }, 2000);
-    if (CurrentDot == NumberSliders){
-        CurrentDot = 0;
-        console.log(CurrentDot);        
+    if (CurrentDot == NumberSliders + 1){
+        CurrentDot = 1;
+               
     }
+    detenerContador();
+    console.log(CurrentDot);
+    DotActive(CurrentDot);
+    iniciarContador();
 }
 
 function SliderPrev(){
@@ -57,45 +58,44 @@ function SliderPrev(){
         Slider.insertAdjacentElement('afterbegin', NewLastItem);
         Slider.style.marginLeft = '-100%';
     }, 2000);
-    if (CurrentDot == -1){
-        CurrentDot = 3;
+    if (CurrentDot == 0){
+        CurrentDot = NumberSliders;
     }    
     detenerContador();
+    console.log(CurrentDot);    
+    DotActive(CurrentDot);
     iniciarContador();
-
+    
 }
 console.log(CurrentDot);
-SliderAuto = setInterval(() => {
-    SliderNext();
-    }, 7000); 
-
-function iniciarContador() {
-    SliderAuto = setInterval(() => {
-    SliderNext();
-    }, 7000); 
+function DotActive(dotactive){
+    const alldots = document.querySelectorAll('.dot');
+    alldots.forEach(element => {
+        if(element.getAttribute('data-dot') == dotactive){
+            element.classList.add('active');
+        } else if(element.classList.contains('active')){
+            element.classList.remove('active');
+        }
+    });
 }
 
-function detenerContador() {
-    clearInterval(SliderAuto);
-}
+
 SliderRight.addEventListener('click',SliderNext);
 Sliderleft.addEventListener('click',SliderPrev);
-
-
-
 
 const Dots = document.querySelector('.dots');
 const NumberDots = ItemsSlider.length;
 
-for (let i = 0; i < NumberDots; i++) {    
+for (let i = 1; i <= NumberDots; i++) {    
     let DotDiv = document.createElement('DIV');
     DotDiv.setAttribute('data-dot',`${i}`)
     DotDiv.classList.add('dot');
+    if(i==1){
+        DotDiv.classList.add('active');
+    }
     Dots.appendChild(DotDiv);
 }
-if (CurrentDot == 6){
-    CurrentDot = 0
-}else if(CurrentDot ==- 1){
-    CurrentDot = 3
-}
+
+
+
 
