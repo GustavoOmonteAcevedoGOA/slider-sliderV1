457b9a64a6822a03dfd769e839fb4a71fb958175
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-07-24 21:38:58 -0400

    dots finished

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ function detenerContador() {
     clearInterval(SliderAuto);
 }
 
+const NumberSliders = 4
+let CurrentDot= 1;
+
+
 const Slider = document.querySelector('.slider');
 const ContainerSlider = document.querySelector('.container-slider');
 const ItemsSlider = document.querySelectorAll('.item-slider');
@@ -17,33 +21,43 @@ const LastItemSlider = ItemsSlider[ItemsSlider.length - 1];
 const SliderRight = document.querySelector('#sliderRight');
 const Sliderleft = document.querySelector('#sliderLeft');
 
-const HeightSlider= '80rem';
-const NumberSliders = 4
-let CurrentDot= 1;
 
-Slider.style.width= `${100 * NumberSliders}%`;
-Slider.style.height = `${HeightSlider}`;
 
 Slider.insertAdjacentElement('afterbegin', LastItemSlider);
 
 function SliderNext(){
     let FirstItemSlider = document.querySelector('.item-slider');
     Slider.style.marginLeft = '-200%';
-    Slider.style.transition = 'all 2s';    
+    Slider.style.transition = 'all 2s';
     CurrentDot+= 1;    
+    
+    //disable Next and Prev while switching photos
+    SliderRight.style.pointerEvents = 'none';
+    Sliderleft.style.pointerEvents = 'none';
+    const disableEveryDot = document.querySelectorAll('.dot');
+    disableEveryDot.forEach(dotdisable => {
+        dotdisable.style.pointerEvents = 'none';
+    });
+
     setTimeout(() => {
         Slider.style.transition = 'none';
         Slider.insertAdjacentElement('beforeend', FirstItemSlider);
         Slider.style.marginLeft = '-100%'
+        //enable Next and Prev
+        SliderRight.style.pointerEvents = 'auto';
+        Sliderleft.style.pointerEvents = 'auto';
+        disableEveryDot.forEach(dotdisable => {
+            dotdisable.style.pointerEvents = 'auto';
+        });
     }, 2000);
     if (CurrentDot == NumberSliders + 1){
-        CurrentDot = 1;
-               
+        CurrentDot = 1; 
     }
+
+    //restart the automatic slider
     detenerContador();
-    console.log(CurrentDot);
     DotActive(CurrentDot);
-    iniciarContador();
+    iniciarContador();    
 }
 
 function SliderPrev(){
@@ -52,22 +66,41 @@ function SliderPrev(){
     Slider.style.marginLeft = '0';
     Slider.style.transition = 'all 2s';
     CurrentDot-=1;
-    
+   
+
+    //disable Next and Prev while switching photos
+    SliderRight.style.pointerEvents = 'none';
+    Sliderleft.style.pointerEvents = 'none';
+    const disableEveryDot = document.querySelectorAll('.dot');
+    disableEveryDot.forEach(dotdisable => {
+        dotdisable.style.pointerEvents = 'none';
+    });
+   
     setTimeout(() => {
         Slider.style.transition = 'none';
         Slider.insertAdjacentElement('afterbegin', NewLastItem);
         Slider.style.marginLeft = '-100%';
+        //enable Next and Prev
+        SliderRight.style.pointerEvents = 'auto'; 
+        Sliderleft.style.pointerEvents = 'auto'; 
+        disableEveryDot.forEach(dotdisable => {
+            dotdisable.style.pointerEvents = 'auto';
+        });
     }, 2000);
     if (CurrentDot == 0){
         CurrentDot = NumberSliders;
-    }    
+    }
+
+    //restart the automatic slider
     detenerContador();
-    console.log(CurrentDot);    
     DotActive(CurrentDot);
     iniciarContador();
     
+  
 }
-console.log(CurrentDot);
+
+
+//highlight the active dot
 function DotActive(dotactive){
     const alldots = document.querySelectorAll('.dot');
     alldots.forEach(element => {
@@ -79,10 +112,11 @@ function DotActive(dotactive){
     });
 }
 
-
 SliderRight.addEventListener('click',SliderNext);
 Sliderleft.addEventListener('click',SliderPrev);
 
+
+//create the displacement dots
 const Dots = document.querySelector('.dots');
 const NumberDots = ItemsSlider.length;
 
@@ -96,6 +130,86 @@ for (let i = 1; i <= NumberDots; i++) {
     Dots.appendChild(DotDiv);
 }
 
+const EveryDot = document.querySelectorAll('.dot');
 
+EveryDot.forEach( (dot) => {
+    dot.addEventListener('click', (e)=>{
+        const targetDot = e.target;
+        const targetDotAttribute = targetDot.getAttribute('data-dot');
+        EveryDot.forEach(dotdisable => {
+            dotdisable.style.pointerEvents = 'none';
+        });
+        if(CurrentDot < targetDotAttribute){
+            let imageJump = targetDotAttribute - CurrentDot; 
+           for (let index = 0; index < imageJump; index++) {
+            let FirstItemSlider = document.querySelector('.item-slider');
+            Slider.style.marginLeft = '-200%';
+            Slider.style.transition = 'all 2s';
+            CurrentDot+= 1;    
+            
+            //disable Next and Prev while switching photos
+            SliderRight.style.pointerEvents = 'none';
+            Sliderleft.style.pointerEvents = 'none';
+            
+                Slider.style.transition = 'none';
+                Slider.insertAdjacentElement('beforeend', FirstItemSlider);
+                Slider.style.marginLeft = '-100%'
+                //enable Next and Prev
+                SliderRight.style.pointerEvents = 'auto';
+                Sliderleft.style.pointerEvents = 'auto';
+            
+            if (CurrentDot == NumberSliders + 1){
+                CurrentDot = 1; 
+            }
+        
+            //restart the automatic slider
+            detenerContador();
+            DotActive(CurrentDot);
+            iniciarContador();
+               
+           }
+           
+        }
+        if(CurrentDot > targetDotAttribute){
 
+            let imageJump = CurrentDot - targetDotAttribute;
+
+            for (let index = 0; index < imageJump; index++) {
+                
+                let SliderNew = document.querySelectorAll('.item-slider');
+                let NewLastItem = SliderNew[SliderNew.length - 1];
+                Slider.style.marginLeft = '0';
+                Slider.style.transition = 'all 2s';
+                CurrentDot-=1;
+               
+            
+                //disable Next and Prev while switching photos
+                SliderRight.style.pointerEvents = 'none';
+                Sliderleft.style.pointerEvents = 'none';
+                    Slider.style.transition = 'none';
+                    Slider.insertAdjacentElement('afterbegin', NewLastItem);
+                    Slider.style.marginLeft = '-100%';
+                    //enable Next and Prev
+                    SliderRight.style.pointerEvents = 'auto'; 
+                    Sliderleft.style.pointerEvents = 'auto'; 
+                
+                if (CurrentDot == 0){
+                    CurrentDot = NumberSliders;
+                }
+            
+                //restart the automatic slider
+                detenerContador();
+                DotActive(CurrentDot);
+                iniciarContador();
+                
+            }
+        }
+        
+        
+        EveryDot.forEach(dotdisable => {
+            dotdisable.style.pointerEvents = 'auto';
+        });
+        
+    });
+});
 
